b3e10eede86fe23261bbf64c43e79f22b8a914ce
leoncan122 <leon.can122@gmail.com>
2023-02-01 11:03:59 +0100

    version 0.0.3

---
+++ src/components/InputDate/InputDate.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./inputDate.css"
+
+interface Props  {
+    children: React.ReactNode;
+    onChange?: React.FormEventHandler<HTMLInputElement>;
+    surveyForm: any;
+    name: any;
+};
+
+const DateComponent = React.forwardRef(({children, surveyForm, ...props}: Props) => {
+  
+  return (
+    <div className="date-input-containter">
+    <label >
+      <h2 className="font-black">
+       {children}
+      </h2>
+      <input 
+      type="date" 
+      {...props} 
+      defaultValue={surveyForm[props.name].split('T')[0]}
+      />
+    </label>
+    </div>
+  );
+});
+
+export default DateComponent;
